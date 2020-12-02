fe8a9b3e8fc13e4feabfa7b3ed4bb7de86ab074b
Aran Reeks <cdtreeks@gmail.com>
2020-12-02 17:43:20 +0000

    Update index.php
    Cleaned up into a single minimal solution

--- day02/index.php
+++ day02/index.php
@@ -1,26 +1,15 @@
 <?php
 
-// Part 1: 460
-$correctPasswords = 0;
+$p1 = $p2 = 0;
 foreach (file('input') as $line) {
-    preg_match('#(?<min>[0-9]+)+\-(?<max>[0-9]+) (?<l>[a-z])\: (?<p>.*)#', $line, $m);
+    preg_match('#(?<min>\d+)+\-(?<max>\d+) (?<l>\w): (?<p>\w+)#', $line, $m);
 
     $occurrences = count_chars($m['p'])[ord($m['l'])];
-    if ($occurrences >= $m['min'] && $occurrences <= $m['max']) {
-        $correctPasswords++;
-    }
+    if ($occurrences >= $m['min'] && $occurrences <= $m['max'])
+        $p1++;
+    if ($m['p'][$m['min']-1] == $m['l'] ^ $m['p'][$m['max']-1] == $m['l'])
+        $p2++;
 }
 
-echo $correctPasswords . PHP_EOL;
-
-// Part 2: 251
-$correctPasswords = 0;
-foreach (file('input') as $line) {
-    preg_match('#(?<p1>[0-9]+)+\-(?<p2>[0-9]+) (?<l>[a-z])\: (?<p>.*)#', $line, $m);
-
-    if ($m['p'][$m['p1']-1] === $m['l'] ^ $m['p'][$m['p2']-1] === $m['l']) {
-        $correctPasswords++;
-    }
-}
-
-echo $correctPasswords . PHP_EOL;
+echo 'Part 1: ' . $p1 . PHP_EOL;
+echo 'Part 2: ' . $p2 . PHP_EOL;
