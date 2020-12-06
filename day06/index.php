dce4e234442644fc0e906d67369010589df36fac
Aran Reeks <cdtreeks@gmail.com>
2020-12-06 13:31:40 +0000

    Use short closures

--- day06/index.php
+++ day06/index.php
@@ -4,14 +4,10 @@ $questions_answered = 0;
 foreach (explode("\n\n", file_get_contents('input')) as $group) {
     $questions_answered += count(array_unique(str_split(str_replace("\n", '', $group))));
 }
-
 echo "Part 1: $questions_answered\n";
 
 $questions_answered = 0;
 foreach (explode("\n\n", file_get_contents('input')) as $group) {
-    $questions_answered += count(array_filter(array_count_values(str_split(str_replace("\n", '', $group))), function($value) use ($group) {
-        return $value === count(explode("\n", $group));
-    }));
+    $questions_answered += count(array_filter(array_count_values(str_split(str_replace("\n", '', $group))), fn($value) => $value == count(explode("\n", $group))));
 }
-
 echo "Part 2: $questions_answered\n";
